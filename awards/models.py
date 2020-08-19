d1fab7a9ca1fb109fc2b197695a047408b0d6c74
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-19 23:20:18 +0300

    Create class project

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=40)
+    description = HTMLField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    link = models.URLField(max_length=200,blank=True)

ef649720956f6c5692008dab9f73c0006b1614a3
Makena-Maryann <maryann.makena00@gmail.com>
2020-08-20 08:18:28 +0300

    Add urls and views

--- awards/urls.py
+++ awards/urls.py
@@ -3,7 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-urlpatterns=[]
+urlpatterns=[
+    url(r'^$',views.index,name = 'index'),
+    url(r'^new/project/$', views.new_project, name='new-project'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^profile/$', views.profile, name='profile'),
+]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- awards/views.py
+++ awards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Project
+from .models import Project,Profile
 from .forms import NewProjectForm
 
 # Create your views here.
@@ -24,4 +24,24 @@ def new_project(request):
 
     else:
         form = NewProjectForm()
-    return render(request,'new_project.html',{"form":form})    
+    return render(request,'new_project.html',{"form":form})
+
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'awards/search.html',{"message":message,"projects":searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'awards/search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user=current_user).all()
+    profile = Profile.objects.filter(user=current_user)
+    return render(request, 'awards/profile.html', {'projects':projects,'profile':profile})
